597c31cd25575a868841d26812206083477b19b9
Nícolas <nicolasanunes@hotmail.com>
2025-08-14 10:42:09 -0300

    github pages implemented

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/codeluni-landing-page/',
   plugins: [vue()],
   server: {
     host: '192.168.10.19'
