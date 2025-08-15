9b71cf3e7f742cd36193031ebb64531cd08ab752
Nícolas <nicolasanunes@hotmail.com>
2025-08-14 22:33:17 -0300

    style(home): changed home page interface

--- src/components/Moon.vue
+++ src/components/Moon.vue
@@ -3,19 +3,26 @@ import { shallowRef } from 'vue'
 import { Levioso, GLTFModel } from '@tresjs/cientos'
 import { useLoop } from '@tresjs/core'
 
+const props = defineProps({
+  isMobile: {
+    type: Boolean,
+    default: false
+  }
+})
+
 const moonRef = shallowRef()
 const { onBeforeRender } = useLoop()
 
 onBeforeRender(() => {
   if (moonRef.value) {
-    moonRef.value.rotation.y += 0.001
+    props.isMobile ? moonRef.value.rotation.y += 0.02 : moonRef.value.rotation.y += 0.002
   }
 })
 </script>
 
 <template>
-  <Levioso speed="3" floatFactor="80">
-    <TresMesh ref="moonRef" :position="[0, 0, 0]" :scale="[0.8, 0.8, 0.8]">
+  <Levioso speed="4" floatFactor="50">
+    <TresMesh ref="moonRef" :position="[0, 0, 0]" :scale="isMobile ? [0.8, 0.8, 0.8] : [1, 1, 1]">
         <GLTFModel
             path="models/moon/scene.gltf"
         />
